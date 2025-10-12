f95fcdab7fdcc88ca82c60af6a3d289746f58096
Cesar Basilio Gomez <108818138+basgomcesar@users.noreply.github.com>
2025-10-12 17:27:27 -0600

    Revert "[WIP] Add Derechohabiente endpoint following CQRS pattern"

--- IPE.Chatbot.Api/Program.cs
+++ IPE.Chatbot.Api/Program.cs
@@ -7,10 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ChatbotDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Add MediatR
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(IPE.Chatbot.Application.Class1).Assembly));
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 

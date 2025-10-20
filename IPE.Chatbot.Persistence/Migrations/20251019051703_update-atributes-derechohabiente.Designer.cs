0e005d3e4b307cf8519f0b138d7ba0528854af6d
basgomcesar <cesarbasiliogomez@gmail.com>
2025-10-19 21:30:55 -0600

    add new atributes

--- IPE.Chatbot.Persistence/Migrations/20251019051703_update-atributes-derechohabiente.Designer.cs
+++ IPE.Chatbot.Persistence/Migrations/20251019051703_update-atributes-derechohabiente.Designer.cs
@@ -4,6 +4,7 @@ using IPE.Chatbot.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IPE.Chatbot.Persistence.Migrations
 {
     [DbContext(typeof(ChatbotDbContext))]
-    partial class ChatbotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251019051703_update-atributes-derechohabiente")]
+    partial class updateatributesderechohabiente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IPE.Chatbot.Persistence/Migrations/20251019051703_update-atributes-derechohabiente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IPE.Chatbot.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateatributesderechohabiente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Afiliacion",
+                table: "Derechohabientes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaCreacion",
+                table: "Derechohabientes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UltimaInteraccion",
+                table: "Derechohabientes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Afiliacion",
+                table: "Derechohabientes");
+
+            migrationBuilder.DropColumn(
+                name: "FechaCreacion",
+                table: "Derechohabientes");
+
+            migrationBuilder.DropColumn(
+                name: "UltimaInteraccion",
+                table: "Derechohabientes");
+        }
+    }
+}
